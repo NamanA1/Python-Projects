2359427fe15f613702fa23d921dd60bb036f750e
Naman Arya <55178429+NamanA1@users.noreply.github.com>
2021-08-23 17:17:06 +0530

    Text mining tool
    To read text input from users and predict its sentiment.

---
+++ pytext ana.py
@@ -0,0 +1,19 @@
+# Python 3 code for text mining using Textblob library.
+from textblob
+import Textblob
+
+y=input("Type your sentence")
+
+edu=TextBlob(y)
+
+x=edu.sentiment.polarity
+
+if x<0:
+   
+ print("Negative")
+
+  elif x=0:
+     
+ print("Neutral")
+
+else print("Positive")
